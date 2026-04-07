ccd5485261292922f3e7c32b44c16c5bd470d784
comradeofjustice <2270874978@qq.com>
2026-04-07 10:09:51 +0000

    chore: remove ignored files from git tracking

--- regesiter.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS mcp_audit_results (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    file_name TEXT NOT NULL,
-    total_tools INTEGER,
-    vulnerable_count INTEGER,
-    raw_json TEXT NOT NULL,
-    created_at TEXT NOT NULL DEFAULT (datetime('now'))
-);
